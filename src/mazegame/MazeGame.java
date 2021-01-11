306e7ab288c932b7083276affb0dc33091844525
Lem2507 <77291783+Lem2507@users.noreply.github.com>
2021-01-11 19:18:18 +0200

    Add files via upload

---
+++ src/mazegame/MazeGame.java
@@ -0,0 +1,38 @@
+package mazegame;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+@SuppressWarnings("serial")
+public class MazeGame extends JFrame {
+ int sizeX=15;
+ int sizeY=15;
+ int cellSize=20;
+ Maze m1=new Maze(sizeX, sizeY);
+ public MazeGame(){
+     initUI();
+ }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+        @Override
+        public void run(){
+            MazeGame mz=new MazeGame();
+            mz.setVisible(true);
+   
+        }
+        });
+    }
+    
+    public void initUI(){
+        setTitle("Maze Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(sizeX*cellSize+200,sizeY*cellSize+75);
+        
+        MazeDisplay md=new MazeDisplay(m1, cellSize);
+        add(md);
+        addKeyListener(md);
+        setContentPane(md);
+        md.setFocusable(true);
+        
+        setLocationRelativeTo(null);
+    }
+}
